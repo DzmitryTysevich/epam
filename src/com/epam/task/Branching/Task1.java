f53e4505142b48f5043822506871f8ebe50fbd02
DzmitryTysevich <72868248+DzmitryTysevich@users.noreply.github.com>
2020-10-19 18:53:20 +0300

    Update Task1.java

--- src/com/epam/task/Branching/Task1.java
+++ src/com/epam/task/Branching/Task1.java
@@ -1,19 +1,23 @@
 package com.epam.task.Branching;
 
 public class Task1 {
-    private static int a;
-    private static int b;
 
-    public static boolean calculateTriangle(int a, int b) {
-        Task1.a=a; Task1.b=b;
-        return (Task1.a + Task1.b) < 180;
+    public static void calculateTriangle(int a, int b) {
+        if ((a + b) < 180) {
+            System.out.print("Triangle ");
+            if (a == 90 || b == 90) {
+                System.out.println("is rectangle");
+            } else {
+                System.out.println("is not rectangle");
+            }
+        } else {
+            System.out.print("Is not triangle");
+        }
+        System.out.println();
     }
 
-    public static boolean calculateRectangle() {
-        return (a + b) + 90 == 180;
-    }
 
     public static void main(String[] args) {
-        System.out.println("Is triangle: " + calculateTriangle(45, 45)+", "+"Is rectangle: " + calculateRectangle());
+        calculateTriangle(90, 30);
     }
 }
