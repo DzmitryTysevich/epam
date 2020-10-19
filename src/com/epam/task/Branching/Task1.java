85a9df1fdd696df958e2914723ebf75fd944de3b
DzmitryTysevich <72868248+DzmitryTysevich@users.noreply.github.com>
2020-10-19 18:56:47 +0300

    Update Task1.java

--- src/com/epam/task/Branching/Task1.java
+++ src/com/epam/task/Branching/Task1.java
@@ -5,7 +5,7 @@ public class Task1 {
     public static void calculateTriangle(int a, int b) {
         if ((a + b) < 180) {
             System.out.print("Triangle ");
-            if (a == 90 || b == 90) {
+            if (a == 90 || b == 90 || a + b == 90) {
                 System.out.println("is rectangle");
             } else {
                 System.out.println("is not rectangle");
