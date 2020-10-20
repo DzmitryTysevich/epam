5593730fcb215073c9939751a60b33a3d9388239
Dzmitry_Tysevich <dztysevich@gmail.com>
2020-10-20 15:53:26 +0300

    Commit

--- src/com/epam/task/Cycles/Task2.java
+++ src/com/epam/task/Cycles/Task2.java
@@ -3,24 +3,22 @@ package com.epam.task.Cycles;
 public class Task2 {
 
     public static void main(String[] args) {
-        System.out.println(calculate(-3, 20, 2.5, -3));
+        calculate(-8, 50, 2.5, 0);
     }
 
-    public static boolean calculate(double a, double b, double h, double x) {
+    public static void calculate(double a, double b, double h, double x) {
         double y;
-        if (x > 2 && a <= x && x <= b) {
-            for (double i = x; i <= b; i += h) {
+        if (x > 2) {
+            for (double i = a; i <= b; i += h) {
                 y = i;
                 System.out.println(y);
             }
-        } else if (x <= 2 && a <= x && x <= b) {
-            for (double i = x; i <= b; i += h) {
-                y = i;
-                System.out.println(y);
-            }
-        } else {
-            return false;
         }
-        return true;
+        if (x <= 2) {
+            for (double i = a; i <= b; i += h) {
+                y = - i;
+                System.out.println(y);
+            }
+        }
     }
 }
