be7d4f261852fbd472e5439b4b139b598cea2a5f
DzmitryTysevich <dztysevich@gmail.com>
2020-10-21 01:07:42 +0300

    Commit

--- src/com/epam/task/module2/oneDimensionalArrays/Task4.java
+++ src/com/epam/task/module2/oneDimensionalArrays/Task4.java
@@ -4,28 +4,23 @@ import java.util.Arrays;
 
 public class Task4 {
     public static void main(String[] args) {
-        calculate(10);
+        calculate(5);
     }
 
     public static void calculate(int n) {
-        int[] a = new int[n];
+        int[] a = {4, 5, 9, 8, 1};
         int max = a[0];
         int min = a[0];
         for (int i = 0; i < a.length; i++) {
-            a[i] = (int) (Math.random() * 100 + 1);
-        }
-
-        for (int i = 0; i < a.length; i++) {
+            // a[i] = (int) ((Math.random() * 100 + 1)+(Math.random() * -100 + 1));
             if (a[i] >= max) {
                 max = a[i];
             }
-            else if (a[i]<=min) {
-                min=a[i];
-                System.out.println(min);
+            if (a[i] <= min) {
+                min = a[i];
             }
         }
-
         System.out.println("Array numbers: " + Arrays.toString(a));
-        System.out.println(max + " ");
+        System.out.println(max + " " + min);
     }
 }
