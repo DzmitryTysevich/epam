99cf8669a1844fcd653672eeee89a40726ebaef4
Dzmitry <72868248+DzmitryTysevich@users.noreply.github.com>
2020-10-26 19:15:32 +0300

    Update Task9.java

--- src/com/epam/task/module2/arraysOfArrays/Task9.java
+++ src/com/epam/task/module2/arraysOfArrays/Task9.java
@@ -1,37 +1,41 @@
 package com.epam.task.module2.arraysOfArrays;
 
+import java.util.Arrays;
 import java.util.Random;
 
-public class Task9 {
+public class Task1 {
     public static void main(String[] args) {
-        calculate(6, 6);
+        calculate(6, 5);
     }
 
     public static void calculate(int x, int y) {
         Random random = new Random();
         int[][] matrix = new int[x][y];
         for (int i = 0; i < matrix.length; i++) {
-            for (int j = 0; j < matrix.length; j++) {
+            for (int j = 0; j < matrix[i].length; j++) {
                 matrix[i][j] = random.nextInt(10);
                 System.out.printf("%d ", matrix[i][j]);
+
             }
             System.out.println();
         }
         System.out.println();
 
-        int sum = 0;
-        int counter = 0;
+        int[] sum = new int[matrix[0].length];
         for (int i = 0; i < matrix.length; i++) {
-            counter++;
-            for (int j = 0; j < matrix.length; j++) {
-                if (j == 0) {
-                    sum += matrix[i][j];
-                    System.out.printf("%d ", matrix[i][j]);
-                }
+            for (int j = 0; j < matrix[i].length; j++) {
+                sum[j] += matrix[i][j];
             }
-            System.out.println();
         }
-        System.out.println();
-        System.out.println(sum);
+        int max = sum[0];
+        int result = 0;
+        for (int i = 0; i < sum.length; i++) {
+            if (sum[i] > max) {
+                max = sum[i];
+                result = i;
+            }
+        }
+        System.out.println(Arrays.toString(sum));
+        System.out.println("Result = " + result);
     }
 }
