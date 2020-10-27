3dc396b894c8231cf93fd8c0d6b8750eb8b18a1d
Dzmitry_Tysevich <dztysevich@gmail.com>
2020-10-27 13:24:36 +0300

    Commit

--- src/com/epam/task/module2/oneDimensionalArrays/Task9.java
+++ src/com/epam/task/module2/oneDimensionalArrays/Task9.java
@@ -11,7 +11,7 @@ public class Task9 {
     public static int[] createRandomArray() {
 
         Random random = new Random();
-        int[] array = {5, 3, 9, 4, 3, 8, 7, 1, 1};
+        int[] array = {5, 3, 9, 3, 4, 8, 7, 1, 1};
 //        for (int i = 0; i < array.length; i++) {
 //            array[i] = (random.nextInt(10) + 1);
 //        }
@@ -19,9 +19,9 @@ public class Task9 {
         return array;
     }
 
-    public static void findPopularValue (int [] array){
+    public static void findPopularValue(int[] array) {
         int[] counterArray = new int[array.length];
-        int popularNumber = array[0];
+
         for (int i = 0; i < array.length; i++) {
             int counter = 1;
             for (int j = i + 1; j < array.length; j++) {
@@ -31,24 +31,25 @@ public class Task9 {
                 }
             }
         }
+        int popularValue = 0;
         int maxCounter = counterArray[0];
         for (int i = 0; i < counterArray.length; i++) {
             if (counterArray[i] >= maxCounter) {
                 maxCounter = counterArray[i];
-                popularNumber = array[i];
+                popularValue = array[i];
                 for (int j = 0; j < counterArray.length; j++) {
                     if (counterArray[j] == maxCounter && counterArray[j] == counterArray[i]) {
-                        if (array[i] < array[j] && array[i] <= popularNumber) {
-                            popularNumber = array[i];
+                        if (array[i] < array[j] && array[i] <= popularValue) {
+                            popularValue = array[i];
                         }
                         if (array[i] > array[j]) {
-                            popularNumber = array[j];
+                            popularValue = array[j];
                         }
                     }
                 }
             }
         }
         System.out.println(Arrays.toString(counterArray));
-        System.out.println("Number: " + popularNumber);
+        System.out.println("Number: " + popularValue);
     }
 }
