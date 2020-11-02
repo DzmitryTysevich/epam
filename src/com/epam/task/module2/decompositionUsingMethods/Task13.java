2e1a945aefcfe05d2b0d2e8665ffedf74aec540f
Dzmitry <72868248+DzmitryTysevich@users.noreply.github.com>
2020-11-02 21:18:31 +0300

    Update Task13.java

--- src/com/epam/task/module2/decompositionUsingMethods/Task13.java
+++ src/com/epam/task/module2/decompositionUsingMethods/Task13.java
@@ -5,17 +5,33 @@ import com.epam.task.Utils.NumberOperation;
 import java.util.Arrays;
 
 public class Task13 {
-    public static void main(String[] args) {
-        System.out.println(Arrays.toString(findSimpleNumbersTwins(1)));
+       public static void main(String[] args) {
+        calculate();
     }
 
-    public static int[] findSimpleNumbersTwins(int startNumber) {
-        int endNumber = 100;
-        int[] array = new int[endNumber];
-        for (int i = startNumber; i < endNumber; i++) {
-            if (endNumber % i == 0)
-                array[i] = i;
+    public static void calculate() {
+        int [] array = new int[20];
+        int counter = 0;
+        for (int i = 1; i < 20; i++) {
+            if (isMaxOneDivider(i) && i != 2) {
+                array[counter]=i;
+                counter++;
+            }
         }
-        return array;
+       int [] array1 = Arrays.copyOf(array, counter);
+        for (int i = 0; i < 7; i++) {
+            if(array1[i+1]-array1[i]==2){
+                System.out.printf("%d %d\n", array1[i], array1[i+1]);
+            }
+        }
+    }
+
+    public static boolean isMaxOneDivider(int number) {
+        for (int i = 2; i < number; i++) {
+            if (number % i == 0) {
+                return false;
+            }
+        }
+        return true;
     }
 }
